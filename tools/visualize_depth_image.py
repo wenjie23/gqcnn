f55d14cc31d274ead182e18d206959caf0044bed
Sippora <wenjieduan23@gmail.com>
2018-09-19 15:16:23 +0200

    Updated tool for visualizing dapth image.

--- tools/visualize_depth_image.py
+++ tools/visualize_depth_image.py
@@ -1,5 +1,6 @@
 """
 A simple script to visulize depth image stored in .npy or .npz file
+Run this script by calling it from command server with --fileName and --ind as arguments
 
 Author
 ------
@@ -11,8 +12,32 @@ import sys
 import os
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
-from matplotlib import cm
 import argparse
+
+def visualize_depth_image(data,titleName=""):
+    """
+    Visualize depth image in blue and red color code and in 3D surface
+    Parameters
+    ---
+    data: obj:numpy.array
+      numpy with shape (height, width, 1)
+    titleName: obj `str`
+      the tile the displayed figure
+    """
+    fig=plt.figure()
+    a=data
+    ny=a.shape[0]
+    nx=a.shape[1]
+    x=np.linspace(0,nx,nx)
+    y=np.linspace(0,ny,ny)
+    xv,yv=np.meshgrid(x,y)
+    ax=fig.add_subplot(111,projection='3d')
+    ax.view_init(azim=-90, elev=-90)
+    if len(a.shape)==3:
+        a=np.squeeze(a,axis=(2,))
+    dem3d=ax.plot_surface(xv,yv,a,cmap=plt.cm.coolwarm,linewidth=0,antialiased=False)
+    fig.colorbar(dem3d,shrink=0.5,aspect=5)
+    plt.title(titleName)
 
 if __name__=="__main__":
     # parse args
@@ -33,16 +58,5 @@ if __name__=="__main__":
         data=raw_data[ind]
 
     # visualize npy file
-    fig=plt.figure()
-    a=data
-    ny=a.shape[0]
-    nx=a.shape[1]
-    x=np.linspace(0,1,nx)
-    y=np.linspace(0,1,ny)
-    xv,yv=np.meshgrid(x,y)
-    ax=fig.add_subplot(111,projection='3d')
-    if len(a.shape)==3:
-        a=np.squeeze(a,axis=(2,))
-    dem3d=ax.plot_surface(xv,yv,a,cmap=cm.coolwarm,linewidth=0,antialiased=False)
-    fig.colorbar(dem3d,shrink=0.5,aspect=5)
+    visualize_depth_image(data,fileName)
     plt.show()
